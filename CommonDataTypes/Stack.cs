3a5f84d2c93baecbe30ad71658cbad0dc09dd6d7
Igromanhic <igromanhicgit@gmail.com>
2024-06-24 18:03:01 +0300

    Basic implementation

--- CommonDataTypes/Stack.cs
+++ CommonDataTypes/Stack.cs
@@ -4,6 +4,49 @@ namespace CommonDataTypes
 {
     public sealed class Stack<T>
     {
+        public int Count => _count;
 
+        private int _count;
+        private StackNode<T> _root;
+
+        public Stack() { }
+
+        public void Push(T item)
+        {
+            StackNode<T> newNode = new StackNode<T>(item);
+
+            if (_root == null)
+            {
+                _root = newNode;
+            }
+            else
+            {
+                newNode.Next = _root;
+                _root = newNode;
+            }
+
+            _count++;
+        }
+
+        public T Pop()
+        {
+            if (_root == null)
+                throw new Exception("Stack is empty");
+
+            T curentItem = _root.Value;
+            _root = _root.Next;
+
+            _count--;
+
+            return curentItem;
+        }
+
+        public T Pick()
+        {
+            if (_root == null)
+                throw new Exception("Stack is empty");
+
+            return _root.Value;
+        }
     }
 }
