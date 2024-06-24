80cddf4768bd5ce8dfd8d3c2266d651cd039a1de
Igromanhic <igromanhicgit@gmail.com>
2024-06-24 17:56:14 +0300

    Basic implementation

--- CommonDataTypes/StackNode.cs
+++ CommonDataTypes/StackNode.cs
@@ -2,6 +2,13 @@
 {
     internal sealed class StackNode<T>
     {
+        public T Value;
+        public StackNode<T> Next;
 
+        public StackNode(T value, StackNode<T> nextNode = null)
+        {
+            Value = value;
+            Next = nextNode;
+        }
     }
 }
