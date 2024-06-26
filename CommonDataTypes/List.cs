dbaf5677a8718d310ba9feb7f8327e814de9d6b5
Igromanhic <igromanhicgit@gmail.com>
2024-06-26 11:16:43 +0300

    Implementation IList<T> and IEnumerator<T>

--- CommonDataTypes/List.cs
+++ CommonDataTypes/List.cs
@@ -1,16 +1,36 @@
 ﻿using System;
+using System.Collections;
+using System.Runtime.CompilerServices;
 
 namespace CommonDataTypes
 {
-    public sealed class List<T>
+    public sealed class List<T> : IList<T>, IEnumerator<T>
     {
         public int Count => _array.Length;
+        public T Current => _array[_enumeratorIndex];
+        object IEnumerator.Current => _array[_enumeratorIndex];
+        public bool IsReadOnly => false;
+        public T this[int index]
+        {
+            get
+            {
+                return _array[index];
+            }
+            set
+            {
+                _array[index] = value;
+            }
+        }
 
         private T[] _array;
+        private int _enumeratorIndex;
+
+        public bool Contains(T item) => IndexOf(item) != -1;
 
         public List()
         {
             _array = new T[0];
+            Reset();
         }
 
         public void Add(T item)
@@ -23,19 +43,28 @@ namespace CommonDataTypes
             _array = newArray;
         }
 
-        public void Remove(T item)
+        public bool Remove(T item)
         {
-            int indexOfElement = Search(item);
+            int indexOfElement = IndexOf(item);
 
             if (indexOfElement == -1)
-                return;
+                return false;
+
+            RemoveAt(indexOfElement);
+            return true;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index >= Count || index < 0)
+                throw new ArgumentOutOfRangeException();
 
             T[] newArray = new T[Count - 1];
 
             int indexNewArray = 0;
             for (int i = 0; i < Count; i++)
             {
-                if (i == indexOfElement)
+                if (i == index)
                     continue;
 
                 newArray[indexNewArray] = _array[i];
@@ -46,7 +75,7 @@ namespace CommonDataTypes
             _array = newArray;
         }
 
-        public void Insert(T item, int index)
+        public void Insert(int index, T item)
         {
             if (index > Count || index < 0)
                 throw new ArgumentOutOfRangeException();
@@ -69,18 +98,47 @@ namespace CommonDataTypes
             _array = newArray;
         }
 
-        public void Clean() => _array = new T[0];
+        public void Clear() => _array = new T[0];
 
-        public T GetByIndex(int index) => _array[index];
-
-        public T SetByIndex(int index, T value) => _array[index] = value;
-
-        public int Search(T key)
+        public int IndexOf(T key)
         {
             for (int i = 0; i < Count; i++)
                 if (_array[i].Equals(key))
                     return i;
             return -1;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if(array == null)
+                throw new ArgumentNullException();
+            if(arrayIndex < 0)
+                throw new ArgumentOutOfRangeException();
+
+            for(int i = 0; i < Count; i++)
+                array[i + arrayIndex] = array[i];
+        }
+
+        public IEnumerator<T> GetEnumerator() => this;
+
+        IEnumerator IEnumerable.GetEnumerator() => this;
+
+        public bool MoveNext()
+        {
+            if(_enumeratorIndex < _array.Length - 1)
+            {
+                _enumeratorIndex++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Reset() => _enumeratorIndex = -1;
+
+        public void Dispose()
+        {
+            _array = null;
         }
 
         private void CopyArrayTo(T[] newArray)
