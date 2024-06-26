a5a9bcdda673bcc497793ba864729e9451177fac
Igromanhic <igromanhicgit@gmail.com>
2024-06-26 12:34:04 +0300

    Basic implementation

--- CommonDataTypes/ListNode.cs
+++ CommonDataTypes/ListNode.cs
@@ -4,6 +4,66 @@ namespace CommonDataTypes
 {
     internal sealed class ListNode<T>
     {
+        public T Value;
+        public ListNode<T> Next;
+        public ListNode<T> Prev;
 
+        public ListNode(T value, ListNode<T> next = null, ListNode<T> prev = null)
+        {
+            Value = value;
+            Next = next;
+            Prev = prev;
+        }
+
+        public void NextConection(ListNode<T> parent)
+        {
+            if (parent.Next == null)
+            {
+                Prev = parent;
+                parent.Next = this;
+            }
+            else
+            {
+                ListNode<T> parentNext = parent.Next;
+
+                Prev = parent;
+                parent.Next = this;
+
+                Next = parentNext;
+                parentNext.Prev = this;
+            }
+        }
+
+        public void PrevConection(ListNode<T> parent)
+        {
+            if (parent.Prev == null)
+            {
+                Next = parent;
+                parent.Prev = this;
+            }
+            else
+            {
+                ListNode<T> parentPrev = parent.Prev;
+
+                Next = parent;
+                parent.Prev = this;
+
+                Prev = parentPrev;
+                parentPrev.Next = this;
+            }
+        }
+
+        public void Disconect()
+        {
+            if(Next != null)
+            {
+                Next.Prev = Prev;
+            }
+
+            if(Prev != null)
+            {
+                Prev.Next = Next;
+            }
+        }
     }
 }
