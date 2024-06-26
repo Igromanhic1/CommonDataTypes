085ab7d3fd6d92c038e0e049e1a6bd54dcc4079e
Igromanhic <igromanhicgit@gmail.com>
2024-06-26 12:34:26 +0300

    Basic implementation

--- CommonDataTypes/LinkedList.cs
+++ CommonDataTypes/LinkedList.cs
@@ -4,6 +4,130 @@ namespace CommonDataTypes
 {
     public sealed class LinkedList<T>
     {
+        public int Count => _count;
 
+        private int _count;
+        private ListNode<T> _root;
+        private ListNode<T> _last;
+
+        public LinkedList() { }
+
+        public void Add(T item)
+        {
+            ListNode<T> newNode = new ListNode<T>(item);
+
+            if(_root == null)
+            {
+                _root = newNode;
+                _last = newNode;
+            }
+            else
+            {
+                newNode.NextConection(_last);
+                _last = newNode;
+            }
+
+            _count++;
+        }
+
+        public void Insert(int index, T item)
+        {
+            if(index < 0 || index > Count)
+                throw new ArgumentOutOfRangeException();
+
+            ListNode<T> newNode = new ListNode<T>(item);
+
+            if (index == 0)
+            {
+                newNode.PrevConection(_root);
+                _root = newNode;
+            }
+            else if(index == Count)
+            {
+                newNode.NextConection(_last);
+                _last = newNode;
+            }
+            else
+            {
+                newNode.PrevConection(GetNodeByIndex(index));
+            }
+
+            _count++;
+        }
+
+        public void Remove(T item) => RemoveAt(IndexOf(item));
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= Count)
+                throw new ArgumentOutOfRangeException();
+
+            if (index == 0)
+            {
+                if (Count == 1)
+                {
+                    Clear();
+                    return;
+                }
+                else
+                {
+                    _root = _root.Next;
+                    _root.Prev = null;
+                }
+            }
+            else if (index == Count-1)
+            {
+                _last = _last.Prev;
+                _last.Next = null;
+            }
+            else
+            {
+                GetNodeByIndex(index).Disconect();
+            }
+
+            _count--;
+        }
+
+        public void Clear()
+        {
+            _root = null;
+            _last = null;
+            _count = 0;
+        }
+
+        public int IndexOf(T item)
+        {
+            int curentIndex = 0;
+            ListNode<T> curentNode = _root;
+            while (curentNode != null)
+            {
+                if(curentNode.Value.Equals(item))
+                    return curentIndex;
+
+                curentNode = curentNode.Next;
+                curentIndex++;
+            }
+
+            return -1;
+        }
+
+        public T GetByIndex(int index) => GetNodeByIndex(index).Value;
+        public void SetByIndex(int index, T value) => GetNodeByIndex(index).Value = value;
+
+        private ListNode<T> GetNodeByIndex(int index)
+        {
+            if (index < 0 || index >= Count)
+                throw new ArgumentOutOfRangeException();
+
+            int curentIndex = 0;
+            ListNode<T> curentNode = _root;
+            while (curentIndex != index)
+            {
+                curentNode = curentNode.Next;
+                curentIndex++;
+            }
+
+            return curentNode;
+        }
     }
 }
