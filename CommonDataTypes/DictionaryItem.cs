f64c33a47b9128072bb917032ae4210070ab0030
Igromanhic <igromanhicgit@gmail.com>
2024-06-28 21:19:10 +0300

    Basic implementation

--- CommonDataTypes/DictionaryItem.cs
+++ CommonDataTypes/DictionaryItem.cs
@@ -2,6 +2,24 @@
 {
     internal sealed class DictionaryItem <TKey, TValue>
     {
+        public TKey Key;
+        public TValue Value;
 
+        public DictionaryItem(TKey key, TValue value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null)
+                throw new ArgumentNullException();
+
+            if (obj is DictionaryItem<TKey, TValue> dict)
+                return Key.Equals(dict.Key) && Value.Equals(dict.Value);
+            else
+                throw new InvalidDataException();
+        }
     }
 }
